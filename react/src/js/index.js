d88f886303ad6b424e1cf8db0905726ab5596f03
hugo <hugoandresdiazbernal@gmail.com>
2020-07-06 23:43:31 -0500

    Mas contenido

---
+++ react/src/js/index.js
@@ -0,0 +1,6 @@
+import '../css/index.css';
+import { render } from 'react-dom';
+import { App } from './components/app';
+import React from 'react';
+
+render(<App />, document.getElementById('container'));
